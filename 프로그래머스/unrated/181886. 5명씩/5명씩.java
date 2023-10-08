9e82c9bb286d339ea2e5e80228db5aa052621bbc
이성제 <devsj3586@gmail.com>
2023-10-08 11:34:47 +0900

    [unrated] Title: 5명씩, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.java
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public List solution(String[] names) {
         List<String> answer = new ArrayList<>();
-        int index = 0;
         for(int i = 0; i < names.length; i++) {
             if(i % 5 == 0) {
                 answer.add(names[i]);
