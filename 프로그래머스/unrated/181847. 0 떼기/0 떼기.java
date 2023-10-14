dbca929b6dfdc3f09f025daf7ad0f886ed9c37fc
이성제 <devsj3586@gmail.com>
2023-10-14 14:55:49 +0900

    [level unrated] Title: 0 떼기, Time: 0.04 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int num = Integer.valueOf(n_str);
+        answer = String.valueOf(num);
+        return answer;
+    }
+}
